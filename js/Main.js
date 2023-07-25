f53779babc8627e41148e43fc0b126bdcb49eb9a
rucian04 <140551353+rucian04@users.noreply.github.com>
2023-07-25 14:29:24 -0500

    commit

---
+++ js/Main.js
@@ -0,0 +1,16 @@
+
+
+
+function suma(){
+    let A = 0;
+    let B = 0;
+    let suma = 0;
+
+
+A = parseInt(prompt ("por favor ingrese el primer valor"));
+B = parseInt(prompt ("por favor ingrese el segundo valor"));
+
+suma = A + B
+
+alert("la suma es igual a: " + suma);
+}
